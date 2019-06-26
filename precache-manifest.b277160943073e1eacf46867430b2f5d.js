e81525d0b7d51e5827aab1c02296a2427a2bcb83
Vignesh Arulvel <vignesh1466@gmail.com>
2019-06-26 21:59:28 +0530

    second commit

---
+++ precache-manifest.b277160943073e1eacf46867430b2f5d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016396e0e25e1b16aa13cf83abf3082f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53280c4c00e514e15449",
+    "url": "/static/css/2.e7f7683d.chunk.css"
+  },
+  {
+    "revision": "7dad37e3036b48094bb6",
+    "url": "/static/css/main.7c9ad518.chunk.css"
+  },
+  {
+    "revision": "53280c4c00e514e15449",
+    "url": "/static/js/2.5661413f.chunk.js"
+  },
+  {
+    "revision": "7dad37e3036b48094bb6",
+    "url": "/static/js/main.afd1da75.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d40e4669287e259d78d8c98164d6ccc7",
+    "url": "/static/media/Augury.d40e4669.png"
+  },
+  {
+    "revision": "4212d635ac0c2edc402d2cc0b3834a71",
+    "url": "/static/media/Cspire.4212d635.png"
+  },
+  {
+    "revision": "3473a5f3074de97a2b55438716f4f5bd",
+    "url": "/static/media/DeviceTracker.3473a5f3.png"
+  },
+  {
+    "revision": "2393d9a995bf3fc2111d6eb06cadabe7",
+    "url": "/static/media/cross.2393d9a9.png"
+  },
+  {
+    "revision": "d813ab5a9ba414f7da400fb98a8bb728",
+    "url": "/static/media/facebook.d813ab5a.png"
+  },
+  {
+    "revision": "15f540bbac67803f32036415358acd9f",
+    "url": "/static/media/header-2-bg.15f540bb.jpg"
+  },
+  {
+    "revision": "2fa277552b620e8275a07a442ea8cd0f",
+    "url": "/static/media/instagram.2fa27755.png"
+  },
+  {
+    "revision": "5f7d502a2da530f2cec1757d7eb5e566",
+    "url": "/static/media/linkedin.5f7d502a.png"
+  },
+  {
+    "revision": "2270f47deb04eed7ded88aabfddb2f1d",
+    "url": "/static/media/profile.2270f47d.jpg"
+  }
+]);
